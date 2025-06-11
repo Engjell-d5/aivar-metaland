97b3552181dada9af54fbf272158fc396dc576f3
RegiLoshi <rloshi111@gmail.com>
2025-06-11 14:10:46 +0200

    Front end for desktop done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import logo from '../assets/logo.png';
+
+const Footer = () => {
+    return (
+        <div className="bg-[#262627] w-full h-[208px]">
+            <div className="flex flex-col justify-center items-between p-8 gap-4">
+                <div className="flex items-center justify-between gap-4">
+                    <img src={logo} alt="logo" className="w-[154px] h-[48px]" />
+                    <div className="flex flex-col text-white">
+                        <span className="font-bold">AIVAR INTL TECHNOLOGIES LIMITED</span>
+                        <span>63-66 Hatton Garden</span>
+                        <span>London, England, EC1N 8LE</span>
+                    </div>
+                </div>
+
+                <div 
+                    className="w-full h-[1px]"
+                    style={{
+                        backgroundImage: 'repeating-linear-gradient(to right, white 0px, white 3px, transparent 3px, transparent 9px)'
+                    }}
+                ></div>
+
+                <div className="w-full flex items-center justify-end underline">
+                    <span className="font-bold">Privacy Policy</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
